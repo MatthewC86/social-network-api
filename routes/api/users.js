e878bc3d7c11fd6c8cfefd237c0b3b969e043f2b
Matthew C <cnymhw86@gmail.com>
2023-03-10 10:30:28 -0500

    added users.js

---
+++ routes/api/users.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUsersById,
+    createUsers,
+    updateUsers,
+    deleteUsers,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/usersController');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUsers);
+
+// /api/users/:userId
+router.route("/:id").get(getUsersById).put(updateUsers).delete(deleteUsers);
+
+// /api/users/:userId/friends/:friendtId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
